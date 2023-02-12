731e48708ef312bc410244aac20486a6fec3764f
Елисеев Дмитрий <120044307+JohhJoja@users.noreply.github.com>
2023-02-12 12:50:57 +0300

    исправил некоторые баги

---
+++ Art_store/Art_store/JS/arm_Engine.js
@@ -0,0 +1,12 @@
+// fist_1 = document.querySelector('.fist_1');
+// let controll = window.addEventListener('mousemove', (e) => {
+//     function widthh(a) {
+//         mainWidth = window.innerWidth;
+//         return a / mainWidth * 100
+//     }
+//     console.log(e.clientX);
+//     console.log(widthh(fist_1.getBoundingClientRect().top) + "%");
+//     console.log(widthh(fist_1.getBoundingClientRect().left) + "%");
+//     fist_1.style.top = `${e.clientY - parseFloat(getComputedStyle(fist_1).height)/2}px`;
+//     fist_1.style.left = `${e.clientX - parseFloat(getComputedStyle(fist_1).width)}px`
+// });
